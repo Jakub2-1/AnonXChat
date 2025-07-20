6928ebbee8b77fefdc14679e5e8e00bebfb1f2fc
Jakub2-1 <jakubvanek70@gmail.com>
2025-07-21 04:26:33 +0930

    index.js

--- index.js
+++ index.js
@@ -124,5 +124,5 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Running on port ${PORT}`));
